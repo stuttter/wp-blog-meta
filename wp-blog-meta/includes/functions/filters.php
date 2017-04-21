395222bbf3f753c696d0d84a38be28d92d3f6221
Jonny Harris <spacedmonkey@users.noreply.github.com>
2017-04-21 20:15:55 +0100

    Cache clear meta (#16)
    * When cleaning site cache, also clear blog meta
    
    * Fix filter
    
    * formatting

---
+++ wp-blog-meta/includes/functions/filters.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * When calling the clean_site_cache function, 
+ * also clean blog meta
+ *
+ * @since 2.0.0
+ */
+function wp_blog_meta_clean_site_cache( $blog_id ) {
+	wp_cache_delete( $blog_id, 'blog_meta' );
+}
+add_action( 'clean_site_cache', 'wp_blog_meta_clean_site_cache' , 10, 1 );
