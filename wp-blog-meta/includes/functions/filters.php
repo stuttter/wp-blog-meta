de4ecd26020571cd0943b6850314384d6ec7df77
Spacedmonkey <jon@spacedmonkey.co.uk>
2017-04-21 17:43:49 +0100

    Fix filter

--- wp-blog-meta/includes/functions/filters.php
+++ wp-blog-meta/includes/functions/filters.php
@@ -11,4 +11,4 @@
 function wp_blog_meta_clean_site_cache( $blog_id ) {
 	wp_cache_delete( $blog_id, 'blog_meta' );
 }
-add_action( 'clean_site_cache', $blog_id, 10, 1 );
+add_action( 'clean_site_cache', 'wp_blog_meta_clean_site_cache' , 10, 1 );
